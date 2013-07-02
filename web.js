3ee341687b87b5b439728df883883643ab9a564d
Ubuntu <ubuntu@ip-10-250-23-105.us-west-2.compute.internal>
2013-07-02 05:51:37 +0000

    part1 for hw1

--- web.js
+++ web.js
@@ -1,12 +1,6 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "A startup is a business built to grow rapidly.\n";
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
