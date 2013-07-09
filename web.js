db8cb47739859c5df7965cf9a95947402d00a6d0
xd1313113 <xd1313113@gmail.com>
2013-07-09 02:35:45 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 var express = require('express');
 
+var fs = require('fs');
+
+var index_file = fs.readFileSync('bit/index.html');
+
+var buffer = new Buffer(index_file);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response){
-    response.send('Hello World!');
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
